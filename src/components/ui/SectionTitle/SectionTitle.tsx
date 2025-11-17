169a93be9a2c7e633955372a5c96377fba3e9525
toimurhasan <2imur.hasan@gmail.com>
2025-11-17 12:22:17 +0600

    category section responsive done

--- src/components/ui/SectionTitle/SectionTitle.tsx
+++ src/components/ui/SectionTitle/SectionTitle.tsx
@@ -5,7 +5,7 @@ interface SectionTitleProps {
 const SectionTitle: React.FC<SectionTitleProps> = ({ title }) => {
   return (
     <div className="bg-[url(/bg-cat.jpg)]  h-[88px] text-white flex justify-center items-center">
-      <h2 className="text-4xl uppercase font-semibold">{title}</h2>
+      <h2 className="lg:text-4xl text-2xl uppercase font-semibold">{title}</h2>
     </div>
   );
 };
