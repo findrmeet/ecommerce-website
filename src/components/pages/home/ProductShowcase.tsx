fc6ef55cd08f5d498de69443c961d3a732595eb3
toimurhasan <2imur.hasan@gmail.com>
2025-11-17 12:49:41 +0600

    responsiveness for product showcase section

--- src/components/pages/home/ProductShowcase.tsx
+++ src/components/pages/home/ProductShowcase.tsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const ProductShowcase = () => {
   return (
-    <div className="grid grid-cols-2 gap-4">
+    <div className="grid lg:grid-cols-2  lg:gap-4 px-2">
       <div className="">
-        <div className="grid grid-cols-2 gap-4">
+        <div className="grid grid-cols-2 gap-2 lg:gap-4">
           <div className="col-span-2 bg-[url(/one.png)] row-span-2 h-[400px] bg-cover"></div>
 
           <img src="three.png" className="" />
