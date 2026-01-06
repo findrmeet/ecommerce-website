feb803a251ea9aea8e79d1db7600dcc5c7f28978
findrmeet <findrmeet@gmail.com>
2026-01-06 11:19:04 +0600

    added sign in page

---
+++ src/app/signin/page.tsx
@@ -0,0 +1,95 @@
+"use client";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { Phone, Lock, Eye } from "lucide-react";
+
+// type declarations
+type SignInValues = {
+  mobile: string;
+  password: string;
+};
+
+export default function SignInForm() {
+  const { register, handleSubmit } = useForm<SignInValues>();
+  const onSubmit: SubmitHandler<SignInValues> = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <section className=" flex items-center justify-center ">
+      <div className=" bg-white rounded-3xl p-6 shadow-xl w-lg">
+        {/* Header */}
+        <div className="text-center mb-8">
+          <h1 className="text-2xl font-semibold">Sign In</h1>
+          <p className="text-sm text-[#8A99AF] mt-1">
+            Enter your credentials to access your account
+          </p>
+        </div>
+
+        <form onSubmit={handleSubmit(onSubmit)} className="space-y-5">
+          {/* Mobile */}
+          <div>
+            <label className="block text-sm font-medium mb-1">
+              Mobile Number
+            </label>
+            <div className="relative ">
+              <Phone className="absolute left-4 top-1/2 -translate-y-1/2 w-4 h-4 text-neutral-400" />
+              <input
+                {...register("mobile")}
+                placeholder="Enter your mobile number"
+                className="w-full pl-11 pr-4 py-3 rounded-xl border border-neutral-300 focus:outline-none  focus:border-green-500"
+              />
+            </div>
+          </div>
+
+          {/* Password */}
+          <div>
+            <label className="block text-sm font-medium mb-1">Password</label>
+            <div className="relative">
+              <Lock className="absolute left-4 top-1/2 -translate-y-1/2 w-4 h-4 text-neutral-400" />
+              <input
+                type="password"
+                {...register("password")}
+                placeholder="Password"
+                className="w-full pl-11 pr-11 py-3 rounded-xl border border-neutral-300 focus:outline-none  focus:border-green-500"
+              />
+              <Eye className="absolute right-4 top-1/2 -translate-y-1/2 w-4 h-4 text-neutral-400 cursor-pointer" />
+            </div>
+          </div>
+
+          {/* Forgot */}
+          <div className="text-sm font-medium text-neutral-700 cursor-pointer">
+            Forgot password?
+          </div>
+
+          {/* Submit */}
+          <button
+            type="submit"
+            className="w-full py-3 bg-green-500 text-white rounded-xl font-semibold hover:bg-green-600 transition cursor-pointer"
+          >
+            Sign In
+          </button>
+
+          {/* Google */}
+          <button
+            type="button"
+            className="w-full py-3 border border-neutral-300 rounded-xl flex items-center justify-center gap-3 hover:bg-neutral-50 transition cursor-pointer"
+          >
+            <img
+              src="https://www.svgrepo.com/show/475656/google-color.svg"
+              className="w-5 h-5"
+            />
+            Sign in with Google
+          </button>
+        </form>
+
+        {/* Footer */}
+        <p className="text-sm text-center text-neutral-400 mt-6">
+          Don’t have an account?{" "}
+          <span className="font-medium text-neutral-900 cursor-pointer">
+            Sign up for free
+          </span>
+        </p>
+      </div>
+    </section>
+  );
+}
